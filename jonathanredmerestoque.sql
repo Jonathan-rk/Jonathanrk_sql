4cb9082d8f5890df9f72f54438112b401c4bd737
Jonathan R. Kalinoski <139520427+Jonathan-rk@users.noreply.github.com>
2025-02-13 18:52:14 -0300

    Update jonathanrkestoque.sql

--- jonathanredmerestoque.sql
+++ jonathanredmerestoque.sql
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `jonathanrkestoque`
 --
+CREATE DATABASE IF NOT EXISTS `jonathanrkestoque` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `jonathanrkestoque`;
 
 -- --------------------------------------------------------
 
